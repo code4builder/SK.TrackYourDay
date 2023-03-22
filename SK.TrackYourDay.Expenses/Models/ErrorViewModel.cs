c80d998e213ae0a58300b17b061987997eb21b10
Sergey Kuleshov <code4builder@gmail.com>
2023-03-22 18:10:59 +0100

    Add project files.

---
+++ SK.TrackYourDay.Expenses/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SK.TrackYourDay.Expenses.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
