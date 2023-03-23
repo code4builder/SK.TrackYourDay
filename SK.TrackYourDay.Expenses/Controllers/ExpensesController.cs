c19dc3b3f93de93b0bab81e71fe178dae05defdf
Sergey Kuleshov <code4builder@gmail.com>
2023-03-23 14:02:21 +0100

    Add Expenses View and Create Button

--- SK.TrackYourDay.Expenses/Controllers/ExpensesController.cs
+++ SK.TrackYourDay.Expenses/Controllers/ExpensesController.cs
@@ -1,10 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SK.TrackYourDay.Expenses.Data;
+using SK.TrackYourDay.Expenses.Models;
 
 namespace SK.TrackYourDay.Expenses.Controllers
 {
     public class ExpensesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public ExpensesController(ApplicationDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
         public IActionResult Index()
+        {
+            IEnumerable<Expense> objList = _db.Expenses;
+            return View(objList);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
