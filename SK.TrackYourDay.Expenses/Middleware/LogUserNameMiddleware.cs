373ceb44385605be903f5a3457318f4e5318a2e6
Sergey Kuleshov <code4builder@gmail.com>
2023-06-26 19:02:26 +0200

    Add UserName to logs

---
+++ SK.TrackYourDay.Expenses/Middleware/LogUserNameMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Serilog.Context;
+
+namespace SK.TrackYourDay.Expenses.Middleware
+{
+    public class LogUserNameMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public LogUserNameMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            LogContext.PushProperty("UserName", context.User.Identity.Name);
+
+            return next(context);
+        }
+    }
+}
