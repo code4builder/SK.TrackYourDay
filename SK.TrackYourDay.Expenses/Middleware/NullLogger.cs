7082d5c6baf7be015d21e9a0bff99f69305a4475
Sergey Kuleshov <code4builder@gmail.com>
2023-07-01 10:44:10 +0200

    EXP-23 <Upd Tests for Expenses Controller>

---
+++ SK.TrackYourDay.Expenses/Middleware/NullLogger.cs
@@ -0,0 +1,14 @@
+﻿namespace SK.TrackYourDay.Expenses.Middleware
+{
+    public class NullLogger<T> : ILogger<T>
+    {
+        public IDisposable BeginScope<TState>(TState state) => null;
+
+        public bool IsEnabled(LogLevel logLevel) => false;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            // Do nothing
+        }
+    }
+}
