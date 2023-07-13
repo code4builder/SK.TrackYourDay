7986b14683d4a67a27acc3a64e21fd6134ee9b60
Sergey Kuleshov <code4builder@gmail.com>
2023-07-13 16:37:36 +0200

    Add interfaces for services

---
+++ SK.TrackYourDay.UseCases/Abstractions/Expenses/Services/IExpenseCategoriesService.cs
@@ -0,0 +1,18 @@
+﻿using SK.TrackYourDay.Domain.Models;
+using SK.TrackYourDay.UseCases.DTOs;
+
+namespace SK.TrackYourDay.UseCases.Abstractions.Expenses.Services
+{
+    public interface IExpenseCategoriesService
+    {
+        Task<List<ExpenseCategoryDTO>> GetAllExpenseCategoriesDTOAsync(string userId);
+        Task<List<ExpenseCategoryDTO>> GetExpenseCategoriesDTOByUserId(string userId);
+        Task<ExpenseCategory> GetExpenseCategoryByIdAsync(int id);
+        Task<ExpenseCategoryDTO> GetExpenseCategoryDTOByIdAsync(int id);
+        Task CreateExpenseCategoryAsync(ExpenseCategoryDTO expenseCategoryDTO, string userId);
+        Task<ExpenseCategory> UpdateExpenseCategoryByIdAsync(int id, ExpenseCategoryDTO expenseCategoryDTO);
+        Task DeleteExpenseCategoryByIdAsync(int id);
+        Task<List<ExpenseCategoryDTO>> GetFriendsExpenseCategoriesAsync(string userId);
+        ExpenseCategoryDTO ConvertExpenseCategoryToDTO(ExpenseCategory expenseCategory, string userId);
+    }
+}
---
+++ SK.TrackYourDay.UseCases/Abstractions/Expenses/Services/IPaymentMethodsService.cs
@@ -0,0 +1,24 @@
+﻿using SK.TrackYourDay.Domain.Models;
+using SK.TrackYourDay.UseCases.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SK.TrackYourDay.UseCases.Abstractions.Expenses.Services
+{
+    public interface IPaymentMethodsService
+    {
+        Task<List<PaymentMethodDTO>> GetAllPaymentMethodsDTOAsync(string userId);
+        Task<List<PaymentMethodDTO>> GetPaymentMethodsDTOByUserId(string userId);
+        Task<PaymentMethod> GetPaymentMethodByIdAsync(int id);
+        Task<PaymentMethodDTO> GetPaymentMethodDTOByIdAsync(int id);
+        Task CreatePaymentMethodAsync(PaymentMethodDTO paymentMethodDTO, string userId);
+        Task DeletePaymentMethodByIdAsync(int id);
+        Task<PaymentMethod> UpdatePaymentMethodByIdAsync(int id, PaymentMethodDTO paymentMethodDTO);
+        Task<List<PaymentMethodDTO>> GetFriendsPaymentMethods(string userId);
+        PaymentMethodDTO ConvertPaymentMethodToDTO(PaymentMethod paymentMethod, string userId);
+
+    }
+}
