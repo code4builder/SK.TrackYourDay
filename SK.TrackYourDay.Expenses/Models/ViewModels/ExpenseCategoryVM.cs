d35e114e7f3807381f05f900d9e7b32f0c325cab
Sergey Kuleshov <code4builder@gmail.com>
2024-05-06 11:44:01 +0200

    Total page upd: upd totals for all years and add totals by category

--- SK.TrackYourDay.Expenses/Models/ViewModels/ExpenseCategoryVM.cs
+++ SK.TrackYourDay.Expenses/Models/ViewModels/ExpenseCategoryVM.cs
@@ -1,14 +1,15 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics;
 
-namespace SK.TrackYourDay.Expenses.Models.ViewModels
+namespace SK.TrackYourDay.Expenses.Models.ViewModels;
+
+[DebuggerDisplay("{Name}")]
+public class ExpenseCategoryVM
 {
-    public class ExpenseCategoryVM
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        public string Name { get; set; }
+    public string Name { get; set; }
 
-        [DisplayName("Created by")]
-        public string User { get; set; } = "Unknown";
-    }
+    [DisplayName("Created by")]
+    public string User { get; set; } = "Unknown";
 }
